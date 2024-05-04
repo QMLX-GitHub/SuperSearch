3ad6b3a74910d7e25b3a70fcbc69031a14a6dbad
qmlx <2300604302@qq.com>
2024-05-04 21:01:57 +0800

    对整个项目进行初始化前后端

---
+++ src/main/java/com/qmlx/springbootinit/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.qmlx.springbootinit.exception;
+
+import com.qmlx.springbootinit.common.BaseResponse;
+import com.qmlx.springbootinit.common.ErrorCode;
+import com.qmlx.springbootinit.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+  
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
---
+++ src/main/java/com/qmlx/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.qmlx.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qmlx.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+  
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
